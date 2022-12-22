36e4ef212dd80e51c543e345302effb4cbd1b93e
Christian Garcia <bones192nd@gmail.com>
2022-12-22 10:30:39 -0500

    commit tree c31ad373ccf5ca1e10be059e2d6fdc4797d731fe

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+
+const dodgerItem = document.getElementById("dodger");
+
+// dodgerItem.style.backgroundColor = "#FF69B4";
+// dodgerItem.style.bottom = "100px";
+
+function moveDodgerLeft(){
+    const leftNumbers = dodgerItem.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if(left > 0) {
+        dodgerItem.style.left = `${left - 1}px`
+    };
+};
+
+
+document.addEventListener('keydown', function(e){
+    if (e.key === "ArrowLeft"){
+        moveDodgerLeft();
+    } else if (e.key === "ArrowRight"){
+        moveDodgerRight();
+    }
+
+});
+
+function moveDodgerRight(){
+    const leftNumbers = dodgerItem.style.left.replace("px","");
+    const left = parseInt(leftNumbers, 10);
+
+    // const itemWidthString = dodgerItem.style.width.replace("px","")
+    // const itemWidth = parseInt(itemWidthString, 10);
+
+    if(left < 400 -40){
+        dodgerItem.style.left = `${left + 1}px`
+    }
+
+};
