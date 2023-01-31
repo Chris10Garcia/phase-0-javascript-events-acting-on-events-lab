d9ead7d53d41421d1202bf4a2da5ede36a7e023a
Christian Garcia <bones192nd@gmail.com>
2023-01-31 13:16:02 -0500

    lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+
+function moveDodgerLeft(){
+    const dodgerElement = document.getElementById('dodger')
+    const positionString = dodgerElement.style.left.replace("px", "");
+    const position = parseInt (positionString, 10);
+    if (position > 0){
+        dodgerElement.style.left = `${position - 1}px`
+    }
+}
+
+function moveDodgerRight(){
+    const dodgerElement = document.getElementById('dodger')
+    const positionString = dodgerElement.style.left.replace("px", "");
+    const position = parseInt (positionString, 10);
+    if (position < 400-40){
+        dodgerElement.style.left = `${position + 1}px`
+    }
+}
+
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === "ArrowLeft"){
+        moveDodgerLeft()
+    } else if (e.key === "ArrowRight"){
+        moveDodgerRight()
+    }
+})
